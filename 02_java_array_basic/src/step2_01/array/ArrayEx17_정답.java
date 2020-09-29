b6893a342a7e78639933db888a12d3d3163787ff
User <User@201-12>
2020-09-29 17:49:11 +0900

    day12(0929) 연습문제

--- 02_java_array_basic/src/step2_01/array/ArrayEx17_정답.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx17_정답.java
@@ -1,4 +1,5 @@
-// 0928월 3교시 17:41-
+// 0928월 3교시 17:41-17:50 + 0929화 1교시 15:06-15:50 + 2교시 16:00-16:29
+// 82분
 package step2_01.array;
 
 import java.util.Scanner;
@@ -22,31 +23,102 @@ import java.util.Scanner;
  * [p1]승리
  * 
  */
-/*
- * 인덱스
- * [0][1][2]
- * [3][4][5]
- * [6][7][8]
- */
 
 public class ArrayEx17_정답 {
-
+	
+	static void printArray(int[] array) { // 오목판 프린트
+//		  인덱스 : 승리하는 경우의 수 : 012/345/678 / 036/147/258 / 048/246
+//		  [0][1][2]
+//		  [3][4][5]
+//		  [6][7][8]
+		System.out.println("=== 틱택토게임중~ ===");
+		for (int i = 0; i < 9; i++) {
+			
+			if(array[i] == 1) System.out.print("[O");
+			else if(array[i] == 2) System.out.print("[X");
+			else if(array[i] == 0) System.out.print("[ ");
+			System.out.print("] ");
+			
+			if(i%3==2) 
+				System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		int[] game = new int[9]; // 1P는 1 2P는 2 빈부분0 
+		int[] game = new int[9]; // 배열안의 값에서 1P는 1 2P는 2 빈부분0 
 		int turn = 1;
 		int usrChoice;
 		
 		while(true) {
+			
+			printArray(game);
+			// 선택하기
 			System.out.print("[p"+turn+"] 인덱스 입력 : ");
+			usrChoice = scan.nextInt();
 		
-		usrChoice = scan.nextInt();
+			if(game[usrChoice] == 0) { 	// 아무도 선택하지 않은 빈 블럭을 선택했다면
+				game[usrChoice] = turn; // 그사람이 선택했다고 표시
+				if(turn == 1) 			// 1p였으면 2p로 순서 바꿔주기
+					turn = 2;
+				else 					// 2p였으면 1p로 순서 바꿔주기
+					turn = 1;
+			} else {
+				System.out.println("이미 선택된 블럭입니다. 다시 고르세요.\n");
+				continue; // 승리여부를 판단하기 전에 while문의 맨앞으로 이동
+			}
+/*
+		// 승리한 인덱스조합을 %3 한 값 : 012/012/012 / 000/111/222 / 012/210
 		
-		if(turn == 1 && game[usrChoice] == 1)
-			System.out.println("이미 선택한 블럭입니다.");
+		// 기타 경우의 수 	%3이 0인 경우 : 036 
+							%3이 1인 경우 : 147 
+							%3이 2인 경우 : 258
+		
+		// 예를들어 %3해서 012값이 같은 경우 - 036중하나 147중하나 258중하나
+		// 012 015 018 042 045 048 072 075 078  
+		// 312 315 318 342 345 348 372 375 378  
+		// 612 615 618 642 645 648 672 675 678  가 같으면 다 승리로 침(나머지로 판단하면)안됨
+*/
+			
+			// 승리했는지 판별하기
+			// 승리하는 경우의 인덱스. 밑의 세 인덱스가 같은 수이면 그 p가 승리  
+			// 012/345/678 / 036/147/258 / 048/246
+			if((game[0] == game[4] && game [0] == game[8] && game[0] != 0) // 048, 246, 345, 147
+					|| (game[2] == game[4] && game [2] == game[6] && game[2] != 0)
+					|| (game[3] == game[4] && game [3] == game[5] && game[3] != 0)
+					|| (game[1] == game[4] && game [1] == game[7] && game[1] != 0)) {
+				printArray(game);
+				System.out.println("["+ game[4] + "p]가 승리하였습니다!!!!!!!!!!! ");
+				break;				
+			} 
+			if((game[0] == game[1] && game [0] == game[2] && game[0] != 0) // 012, 036
+					||(game[0] == game[3] && game [0] == game[6] && game[0] != 0)) {
+				printArray(game);
+				System.out.println("["+game[0] + "p]가 승리하였습니다!!!!!!!!!!! ");
+				break;				
+			}
+			if((game[8] == game[7] && game [8] == game[6] && game[8] != 0) // 678, 258
+					||(game[8] == game[2] && game [8] == game[5] && game[8] != 0)) {
+				printArray(game);
+				System.out.println("["+game[8] + "p]가 승리하였습니다!!!!!!!!!!! ");
+				break;				
+			}	
 		}
-		
+		scan.close();
 	}
-		
 }
+
+/*
+ * 
+가로 for문 한개
+ if ( 0 > 3개 ) 1p win
+ if (1 > 3개 ) 2p win
+
+세로 for문 한개
+ 1p,2p
+
+대각선
+ 1p,2p
+
+*/
--- 02_java_array_basic/src/step2_01/array/ArrayEx18_정답2.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx18_정답2.java
@@ -1,7 +1,9 @@
-// 0929화요일 2-3교시 16:30-16:50 + 17:00-17:30 = 총 50분
+// 0929화요일 3교시 17:30-17:42 = 총 12분
+// 앞에 푼 정답1은 p1과 p2가 둘다 인간인경우이고 정답2는 p2를 컴퓨터로 만들어 랜덤으로 게임진행하는 경우
 package step2_01.array;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -29,11 +31,11 @@ import java.util.Scanner;
  * [p1사용자]1~3 입력 : 
  */
 
-// 따라잡았다면 상대turn 말을 원점으로 이동 
-public class ArrayEx18_정답 {
+public class ArrayEx18_정답2 {
 
 	public static void main(String[] args) {
 		
+		Random ran = new Random();
 		Scanner scan = new Scanner(System.in);
 		
 		int[] game = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -42,21 +44,16 @@ public class ArrayEx18_정답 {
 		int[] p2   = {0, 0, 0, 0, 0, 0, 0, 0};
 		
 		int turn = 1; // turn 이 1이면 p1, 2면 p2의 차례
-		int move;
+		int move =0;
 
-		// 각각 말의 초기 위치 설정
-		int idx1 = 0;	
-		int idx2 = 0; 
-		
-		// 각각 말의 위치에 놓기
-		p1[idx1] = 1;	
-		p2[idx2] = 2; 
+		// 각각 말의 초기 위치 설정 + 각각 말의 위치에 놓기 
+		int idx1 = 0;	p1[idx1] = 1;	
+		int idx2 = 0; 	p2[idx2] = 2; 
 		
 		// 각자 이긴 바퀴수
-		int win1 = 0;	
-		int win2 = 0;	
+		int win1 = 0;	int win2 = 0;	
 		
-		System.out.println("친구와 함께하는 미니마블!");
+		System.out.println("컴퓨터와 함께하는 미니마블!");
 		
 		do {
 			// 프린트하기
@@ -64,17 +61,24 @@ public class ArrayEx18_정답 {
 			System.out.println(Arrays.toString(p1) + "["+win1+"바퀴]");
 			System.out.println(Arrays.toString(p2) + "["+win2+"바퀴]");
 			
-			// 몇칸을 이동할까요? 입력받기
-			System.out.print("[p" + turn + "] 1~3 입력 : ");
-			move = scan.nextInt();
-			
-			if(move > 3 || move <= 0) {
-				System.out.println("1~3 사이의 값만 입력하십시오!\n");
-				continue;
-			} 
+			// 몇칸을 이동할까요? 
+			// p1에게는 입력받고 p2는 컴퓨터이므로 랜덤진행 
+			// 정답2에서 랜덤인 경우를 위해 이부분만 수정하였고 이동부터는 수정하지 않았음.
+			if(turn == 1) {
+				System.out.print("[p1] 1~3 입력 : ");
+				move = scan.nextInt();
+				if(move > 3 || move <= 0) {
+					System.out.println("1~3 사이의 값만 입력하십시오!\n");
+					continue;
+				} 
+			} else if(turn == 2) {
+				move = ran.nextInt(3)+1;
+				System.out.println("\n[p2] 1~3 랜덤입력 : " + move);
+				
+			}			
 			System.out.println();
 			
-			// 일단 이동시키기
+			// 일단 이동시키기 
 			if(turn == 1) { // 1의 말이이동하는데 따라잡지 않으면 (이동만)
 				p1[idx1] = 0; 			// 원래 말 위치에 0으로 값바꾸고 
 				idx1 += move;			// 말 이동
@@ -127,7 +131,7 @@ public class ArrayEx18_정답 {
 		else 
 			System.out.print("비정상적인 ");
 			
-		System.out.println(" 게임종료!!!\n");
+		System.out.println("게임종료!!!\n");
 		scan.close();
 	}
 }
